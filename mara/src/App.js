5cc1414051a849fec9efbf2b1137517ebcef89d4
Ryula <hobby519@naver.com>
2022-11-06 04:17:55 +0900

    page move

--- mara/src/App.js
+++ mara/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom"
 import SignUp from "./commons/components/sign_up/sign_up"
 import Menu from "./commons/components/menu"
 import Main from "./commons/components/main"
@@ -7,12 +7,13 @@ import Main from "./commons/components/main"
 function App() {
   return (
     <div className="App">
-      아이디:
       <BrowserRouter>
-      <Menu></Menu>
+        <Link to="/">main</Link>
+        <Link to="/sign_up">회원가입</Link>
+        <Link to="/login">로그인</Link>
         <Routes>
-          <Route path="/" exact element={<Main/>}></Route>
-          <Route path="/sign_up" exact element={<SignUp/>}></Route>
+          <Route path="/" element={<Main/>}></Route>
+          <Route path="/sign_up" element={<SignUp/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
