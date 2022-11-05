623c1eb1769b0f6e4afb88507ac00a194c4009ca
Ryula <hobby519@naver.com>
2022-11-06 06:04:48 +0900

    signup css

--- mara/src/commons/components/sign_up/select_me.js
+++ mara/src/commons/components/sign_up/select_me.js
@@ -1,8 +1,15 @@
-import list from '../../data/caktail';
+import list from '../../data/caktail'
+import '../../../assets/css/select_me.css'
+import {Link} from "react-router-dom"
 
 function select_me(){
     return(
-        <>
+        <div id="me">
+            <div id="level">
+                <div id="square"></div>
+                <div id="square_empty"></div>
+            </div>
+            
             <h1>좋아하는 칵테일을 3개 선택 해주세요</h1>
             <image>
             {
@@ -13,7 +20,8 @@ function select_me(){
                 );
             })}
             </image>
-        </>
+            <button><Link style={{textDecoration: 'none', color: 'white'}} to="/sign_up/page3">다음 → </Link></button><br/>
+        </div>
     );
 }
 
--- mara/src/commons/components/sign_up/sign_up.js
+++ mara/src/commons/components/sign_up/sign_up.js
@@ -5,12 +5,10 @@ import {Link} from "react-router-dom"
 function SignUp(){
     return(
         <div id="sign_up">
-            <contents>
-                ID <input placeholder="아이디를 입력하세요."/><br/>
-                Nickname <input placeholder="닉네임을 입력하세요."/><br/>
-                Password <input placeholder="비밀번호를 입력하세요."/><br/>
-                <button><Link to="/sign_up/page2">다음 → </Link></button><br/>
-            </contents>
+            ID <input placeholder="아이디를 입력하세요."/><br/>
+            Nickname <input placeholder="닉네임을 입력하세요."/><br/>
+            Password <input placeholder="비밀번호를 입력하세요."/><br/>
+            <button><Link style={{textDecoration: 'none', color: 'white', fontSize: '20px'}} to="/sign_up/page2">다음 → </Link></button><br/>
         </div>
     );
 }
