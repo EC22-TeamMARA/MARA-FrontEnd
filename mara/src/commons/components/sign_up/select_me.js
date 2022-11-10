3ff9fe622ddc3696083ddacf6faf546d4dc30660
Ryula <hobby519@naver.com>
2022-11-10 17:11:56 +0900

    merge css modification

--- mara/src/commons/components/sign_up/select_me.js
+++ mara/src/commons/components/sign_up/select_me.js
@@ -1,13 +1,13 @@
 import list from '../../data/caktail'
-import '../../../assets/css/select_me.css'
+import styles from '../../../assets/css/select_me.module.css'
 import {Link} from "react-router-dom"
 
 function select_me(){
     return(
-        <div id="me">
-            <div id="level">
-                <div id="square"></div>
-                <div id="square_empty"></div>
+        <div id={styles.me}>
+            <div id={styles.level}>
+                <div id={styles.square}></div>
+                <div id={styles.square_empty}></div>
             </div>
             
             <h1>좋아하는 칵테일을 3개 선택 해주세요</h1>
@@ -16,11 +16,11 @@ function select_me(){
                 list.map((Item)=>{
                 var {id, img}=Item;
                 return(
-                    <img src={img} alt={id}/>
+                    <img className={styles.me} src={img} alt={id}/>
                 );
             })}
             </image>
-            <button><Link style={{textDecoration: 'none', color: 'white'}} to="/sign_up/page3">다음 → </Link></button><br/>
+            <button><Link style={{textDecoration: 'none', color: 'white', fontSize: '20px'}} to="/sign_up/page3">다음 → </Link></button><br/>
         </div>
     );
 }
--- mara/src/commons/components/sign_up/sign_up.js
+++ mara/src/commons/components/sign_up/sign_up.js
@@ -1,13 +1,13 @@
-import "../../../assets/css/sign_up.css"
+import styles from  "../../../assets/css/sign_up.module.css"
 import {Link} from "react-router-dom"
 
 
 function SignUp(){
     return(
-        <div id="sign_up">
-            ID <input placeholder="아이디를 입력하세요."/><br/>
-            Nickname <input placeholder="닉네임을 입력하세요."/><br/>
-            Password <input placeholder="비밀번호를 입력하세요."/><br/>
+        <div id={styles.sign_up}>
+            <imfor>ID</imfor> <input placeholder="아이디를 입력하세요."/><br/>
+            <imfor>Nickname</imfor> <input placeholder="닉네임을 입력하세요."/><br/>
+            <imfor>Password</imfor> <input placeholder="비밀번호를 입력하세요."/><br/>
             <button><Link style={{textDecoration: 'none', color: 'white', fontSize: '20px'}} to="/sign_up/page2">다음 → </Link></button><br/>
         </div>
     );
