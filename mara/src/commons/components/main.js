b391c21e16ca6a9c71a92edfc8a911e62cfce9b5
Ryula <hobby519@naver.com>
2022-11-19 16:02:34 +0900

    [FEAT] main image

--- mara/src/commons/components/main.js
+++ mara/src/commons/components/main.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import styles from "../../assets/css/main.module.css";
+
 
 function Main(){
 
     return(
         <div>
             이건 main 페이지<br/>
+            <img className={styles.img_main} src="/drink.png" alt="drink_cock"/>
             공사중...
         </div>
     );
